4b3968b78f8764c2ff1580249a08893966776009
YouTube <abdullahdeveloper05@gmail.com>
2024-12-21 17:51:04 +0500

    Imagify

---
+++ client/src/components/Description.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { assets } from "../assets/assets";
+import { motion } from "motion/react";
+
+const Description = () => {
+  return (
+    <motion.div
+      initial={{ opacity: 0.2, y: 100 }}
+      transition={{ duration: 1 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      viewport={{ once: true }}
+      className="flex flex-col items-center justify-center my-24 p-6 md:px-28"
+    >
+      <h1 className="text-3xl sm:text-4xl font-semibold mb-2">
+        Create AI Images
+      </h1>
+      <p className="text-gray-500 mb-8">Turn your imagination into visuals</p>
+
+      <div className="flex flex-col gap-5 md:gap-14 md:flex-row items-center">
+        <img src={assets.sample_img_1} className="w-80 xl:w-96 rounded-lg" />
+        <div>
+          <h2 className="text-3xl font-medium max-w-lg mb-4">
+            Introducing the AI-Powered Text to Image Genetator
+          </h2>
+          <p className="text-gray-600 mb-4">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque
+            officiis, aut consectetur accusamus aperiam non possimus perferendis
+            tempore itaque dolorem, fugit repudiandae libero animi nihil nisi
+            voluptate amet ab enim.
+          </p>
+          <p className="text-gray-600 mb-4">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rerum sint
+            cumque tempora deserunt omnis magnam modi perspiciatis nesciunt
+            deleniti alias! Velit itaque similique deleniti, vel perferendis
+            exercitationem numquam. Quis consectetur exercitationem officiis
+            facere tempore dignissimos!
+          </p>
+        </div>
+      </div>
+    </motion.div>
+  );
+};
+
+export default Description;
